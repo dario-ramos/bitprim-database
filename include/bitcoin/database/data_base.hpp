82fb912185a21dded556f2939ee3cca408905725
fpelliccioni <fpelliccioni@gmail.com>
2016-10-20 15:17:02 -0300

    misc

--- include/bitcoin/database/data_base.hpp
+++ include/bitcoin/database/data_base.hpp
@@ -158,8 +158,8 @@ public:
     history_database history;
     spend_database spends;
 
-    unspent_database unspents;
-    // unspent_database_v2 unspents;
+    //unspent_database unspents;
+    unspent_database_v2 unspents;
     // std::unique_ptr<unspent_database_v2> unspents;
 
     stealth_database stealth;
